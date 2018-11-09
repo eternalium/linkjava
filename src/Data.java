bd8aa4f3fa5fdf8d41e6e8488b73b49fd1947903
StefanBadea <stefanrbadea@yahoo.com>
2018-11-09 18:02:01 +0200

    import

---
+++ src/Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge = 32;
+        float weight = 101.5f;
+        double height = 1.90;
+        long distance = 3124234234234232342l;
+        char gender = 'm';
+        char test = 400;
+        boolean isStudent = true;
+        boolean hasJob = false;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.print(test2);
+    }
+}
